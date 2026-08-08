65d1a87f15df5ea1dc742ec1aebb884da591ff56
deltamax26 <deltaf@deltaf.site>
2026-08-08 18:13:11 -0400

    Simplify dropdown menu display toggle
    Refactor dropdown menu toggle logic for better clarity.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.querySelector('.dropdown').addEventListener('click', function(){
+const menu = this.querySelector('.dropdown-menu');
 
-    const menu = this.querySelector('.submenu');
-
-    if(menu.style.display === 'block'){
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
-    }
+if(menu.style.display === 'flex'){
+menu.style.display='none';
+}else{
+menu.style.display='flex';
+}
 
 });
